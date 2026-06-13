6e79d8594a59eeb03c1b41d55e2ff958d6b93e00
Owen Arsene <4h9mfbztvw@privaterelay.appleid.com>
2026-06-13 12:16:00 +0200

    Add files via upload

---
+++ 1. Beginner Queries.sql
@@ -0,0 +1,95 @@
+Select TOP 10*
+FROM AthleteData;
+
+--Question 1--
+--How many athletes are in the dataset?--
+SELECT COUNT(Athlete_ID) As Athlete Count
+FROM AthleteData;
+
+--Question 2--
+--What is the average Performance Score across all athletes?--
+SELECT	AVG(Performance_Score) AS AVGPerformance
+FROM AthleteData;
+
+--Question 3--
+--How many athletes have a Fatigue Score greater than 80?--
+SELECT COUNT(Athlete_ID) 
+FROM AthleteData
+WHERE Fatigue_Score >80;
+
+--Question 4--
+--Show the 5 athletes with the highest Performance Score.--
+SELECT TOP 5
+    Athlete_ID,
+    Performance_Score
+FROM AthleteData
+ORDER BY Performance_Score DESC;
+
+--Question 5--
+SELECT DISTINCT Training_Intensity
+FROM AthleteData;
+
+--Question 6--
+--How many athletes belong to each Gender?--
+SELECT
+    Gender,
+    COUNT(*) AS Total
+FROM AthleteData
+GROUP BY Gender;
+
+--Question 7--
+-- Which Training Intensity has the highest average Fatigue Score?--
+SELECT
+		Training_Intensity,
+		AVG(Fatigue_score)	AS AVGFatigueScore
+FROM AthleteData
+GROUP BY Training_Intensity
+ORDER BY AVG(Fatigue_score)	DESC;
+
+--Question 8--
+--Find all athletes who: Have Fatigue Score > 75 AND Performance Score < 50 - How many are there?--
+SELECT 
+		COUNT(Athlete_ID) 
+FROM AthleteData
+WHERE Fatigue_Score >7
+	AND Performance_Score <5;
+
+--Question 9--
+--What is the average ACL Risk Score by Gender?--
+
+SELECT 
+		GENDER,
+		AVG(ACL_Risk_Score) AS AVG_ACL_Risk
+FROM AthleteData
+GROUP BY Gender;
+
+--Question 10--
+--Count how many athletes fall into each category. / fatigue level = High - 8, Medium 5-7, Low >5--
+WITH FatigueCategories AS (
+    SELECT
+        Athlete_ID,
+        CASE
+            WHEN Fatigue_Score >= 8 THEN 'High'
+            WHEN Fatigue_Score >= 5 THEN 'Medium'
+            ELSE 'Low'
+        END AS FatigueLevel
+    FROM AthleteData
+)
+SELECT 
+		FatigueLevel,
+		COUNT(Athlete_ID) AS Athelete_count
+FROM FatigueCategories
+GROUP BY FatigueLevel;
+
+
+--Questoion 11--
+--Create 1 summary table showing - training intensity, number of atheltes, avg performance, avg fatigue, avg fatigue, avg acl risk--
+SELECT 
+		Training_Intensity,
+		COUNT(Athlete_ID) AS Numb_of_atheltes,
+		AVG(Performance_Score) AS AVGPerformance_Score,
+		AVG(Fatigue_Score) AS AVGFatigue_Score,
+		AVG(ACL_Risk_Score) AS AVGACL_Risk_Score
+FROM AthleteData
+GROUP BY Training_Intensity
+ORDER BY AVG(Performance_Score) DESC;
---
+++ 2. Intermiediate Queries.sql
@@ -0,0 +1,151 @@
+SELECT TOP 10 *
+FROM Athletedata;
+
+--Question 1--
+--Which Training Intensity has the highest average performance score?--
+SELECT 
+		Training_Intensity,
+		AVG(Performance_Score) AS AVGPerformance_Score
+FROM Athletedata
+GROUP BY Training_Intensity
+ORDER BY AVG(Performance_Score) DESC; 
+
+--Question 2--
+--Find the top 10 athletes with the highest ACL risk--
+SELECT TOP 10 Athlete_ID, ACL_Risk_Score   
+FROM Athletedata
+ORDER BY ACL_Risk_Score DESC;  
+
+--Question 3--
+--What is the average heartbeat by Gender?--
+SELECT 
+		Gender,
+		AVG(heartbeat) AS AVGHeartBeat
+FROM Athletedata
+GROUP BY Gender;
+
+--Question 4--
+--Which Gender has the highest average fatigue score?--
+SELECT 
+		Gender,
+		AVG(Fatigue_Score)
+FROM Athletedata
+GROUP BY Gender
+ORDER BY AvgFatigue DESC;
+
+--Question 5--
+--Create a Performance Category--
+WITH PerformanceCategory AS (
+	SELECT Athlete_ID,
+		CASE	
+			WHEN Performance_Score >= 8 THEN 'Elite'
+			WHEN Performance_Score >= 7 THEN 'Good'
+			ELSE 'Average'
+		END AS PerformanceLevel
+FROM Athletedata
+)
+SELECT 
+		PerformanceLevel,
+		COUNT(Athlete_ID) AS Athlete_Count
+FROM PerformanceCategory
+GROUP BY PerformanceLevel
+ORDER BY COUNT(Athlete_ID) DESC;
+
+--Question 6--
+--Create an Injury High Risk Category--
+WITH InjuryHighRiskCategory AS (
+	SELECT Athlete_ID,
+		CASE 
+			WHEN ACL_Risk_Score >= 7 THEN 'High Risk'
+			WHEN ACL_Risk_Score >= 6 THEN 'Medium'
+			ELSE 'Low Risk'
+		END AS ACLRisk
+	FROM Athletedata
+)
+
+SELECT 
+		ACLRisk,
+		COUNT(Athlete_ID) AS Athlete_Count
+FROM InjuryHighRiskCategory
+GROUP BY ACLRisk
+ORDER BY COUNT(Athlete_ID) DESC;
+
+--Question 7--
+--Create a Training Intensity--
+SELECT
+    Training_Intensity,
+    COUNT(
+        CASE
+            WHEN Injury_Indicator = 1	THEN 1
+        END
+    ) AS InjuredAthletes,
+
+    COUNT(
+        CASE
+            WHEN  Injury_Indicator = 0	THEN 1
+        END
+    ) AS NonInjuredAthletes
+FROM AthleteData
+GROUP BY Training_Intensity;
+
+--Question 8--
+--Show only Training Intensities whose average fatigue score is greater or equal to 5--
+SELECT 
+		Training_Intensity,
+		AVG(Fatigue_Score) AS AVGFatigueScore
+FROM AthleteData
+GROUP BY Training_Intensity
+HAVING AVG(Fatigue_Score) >= 5
+ORDER BY AVG(Fatigue_Score) DESC;
+
+--Question 9--
+--Show Genders with more than 200 athletes--
+
+SELECT
+		Gender,
+		COUNT(Athlete_ID) AS Athlete_Count
+FROM AthleteData
+GROUP BY Gender
+HAVING COUNT(Athlete_ID) > 200;
+
+--Question 10--
+--Calculate the avg performance for each fatigue level category--
+WITH FatigueCategories AS (
+    SELECT
+		Performance_Score,
+        Athlete_ID,
+        CASE
+            WHEN Fatigue_Score >= 8 THEN 'High'
+            WHEN Fatigue_Score >= 5 THEN 'Medium'
+            ELSE 'Low'
+        END AS FatigueLevel
+    FROM AthleteData
+)
+SELECT 
+		FatigueLevel,
+		AVG(Performance_Score) AS AVGPerformance
+FROM FatigueCategories
+GROUP BY FatigueLevel;
+	
+--Question 11--
+--count athletes in each fatigue level--
+WITH FatigueCategories AS (
+    SELECT
+        Athlete_ID,
+        CASE
+            WHEN Fatigue_Score >= 8 THEN 'High'
+            WHEN Fatigue_Score >= 5 THEN 'Medium'
+            ELSE 'Low'
+        END AS FatigueLevel
+    FROM AthleteData
+)
+SELECT 
+		FatigueLevel,
+		COUNT(Athlete_ID) AS Athelete_Count
+FROM FatigueCategories
+GROUP BY FatigueLevel
+ORDER BY COUNT(Athlete_ID) DESC;
+
+
+
+	
